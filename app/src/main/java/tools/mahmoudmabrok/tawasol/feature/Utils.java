3968cb8c0a20427579743fbfe0055fea69e60eaa
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2019-12-02 20:03:51 +0200

    initial commit

---
+++ app/src/main/java/tools/mahmoudmabrok/tawasol/feature/Utils.java
@@ -0,0 +1,36 @@
+package tools.mahmoudmabrok.tawasol.feature;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Utils {
+    public static void hideInputKeyboard(Context context) {
+        InputMethodManager imm = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.CUPCAKE) {
+            imm = (InputMethodManager) context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            imm.toggleSoftInput(InputMethodManager.RESULT_HIDDEN, 0);
+        }
+    }
+
+    /**
+     * checks if the device is connected to the internet or not
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+}
